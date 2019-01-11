d3dc167421e29eccbeb1beca18b184119bf33bad
Adam Marczyk <adam.marczyk90@gmail.com>
2019-01-12 00:00:36 +0100

    add bidiractional flow

--- src/main/java/com/github/adammarczyk/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/adammarczyk/grpc/greeting/client/GreetingClient.java
@@ -5,6 +5,7 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -22,15 +23,14 @@ public class GreetingClient {
                 .usePlaintext()
                 .build();
         // doUnaryCall(channel);
-//        doServerStreamingCall(channel);
-        doClientStreamingCall(channel);
+        // doServerStreamingCall(channel);
+        // doClientStreamingCall(channel);
+        doBidiStreamingCall(channel);
 
         System.out.println("Shutting down channel");
         channel.shutdownNow();
 
     }
-
-
 
     private void doUnaryCall(ManagedChannel channel) {
 
@@ -115,6 +115,47 @@ public class GreetingClient {
 
         try {
             latch.await(3L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doBidiStreamingCall(ManagedChannel channel) {
+
+        // create an async client
+        GreetServiceGrpc.GreetServiceStub asyncClient = GreetServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<GreetEveryoneRequest> requestObserver = asyncClient.greetEveryone(new StreamObserver<GreetEveryoneResponse>() {
+            @Override
+            public void onNext(GreetEveryoneResponse value) {
+                System.out.println("Response from server: " + value.getResult());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server is done sending data");
+                latch.countDown();
+            }
+        });
+
+        Arrays.asList("Adam", "John", "Marcy", "Patricia").forEach(
+                name -> {
+                    System.out.println("Sending " + name);
+                    requestObserver.onNext(GreetEveryoneRequest.newBuilder().setGreeting(Greeting.newBuilder().setFirstName(name)).build());
+                }
+        );
+
+        requestObserver.onCompleted();
+
+        try {
+            latch.await(3, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/main/java/com/github/adammarczyk/grpc/greeting/server/GreetServiceImpl.java
+++ src/main/java/com/github/adammarczyk/grpc/greeting/server/GreetServiceImpl.java
@@ -3,6 +3,8 @@ package com.github.adammarczyk.grpc.greeting.server;
 import com.proto.greet.*;
 import io.grpc.stub.StreamObserver;
 
+import java.util.stream.Stream;
+
 public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
 
     @Override
@@ -66,6 +68,30 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
                                 .setResult(result)
                                 .build()
                 );
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    @Override
+    public StreamObserver<GreetEveryoneRequest> greetEveryone(StreamObserver<GreetEveryoneResponse> responseObserver) {
+
+        return new StreamObserver<GreetEveryoneRequest>() {
+            @Override
+            public void onNext(GreetEveryoneRequest value) {
+                String result = "Hello " + value.getGreeting().getFirstName();
+                GreetEveryoneResponse greetEveryoneResponse = GreetEveryoneResponse.newBuilder().setResult(result).build();
+
+                responseObserver.onNext(greetEveryoneResponse);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // do nothing
+            }
+
+            @Override
+            public void onCompleted() {
                 responseObserver.onCompleted();
             }
         };
